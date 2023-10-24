105f906aee11a2739c966c88627b41b88a428e38
ParkSuCheol <3864617@naver.com>
2023-10-25 00:55:47 +0900

    feat : BaseballGame , Refree 기능 생성  - BaseballGame 에서는 Ball, String 카운트 체크  - Refree 에서는 게임이 끝났는지 여부 체크

--- src/main/java/domain/BaseballGame.java
+++ src/main/java/domain/BaseballGame.java
@@ -1,4 +1,46 @@
 package domain;
 
 public class BaseballGame {
+    private int ball;
+    private int strike;
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int[] getNumberCompareResult(String userNumbers, String answerNumbers) {
+        resetCount();
+        ballCount(userNumbers, answerNumbers);
+        strikeCount(userNumbers, answerNumbers);
+        strikeExceptBall();
+
+        return new int[]{ball, strike};
+    }
+
+    private void strikeExceptBall() {
+        ball = ball - strike;
+    }
+
+    private void strikeCount(String userNumbers, String answerNumbers) {
+        for (char userNumber : userNumbers.toCharArray()) {
+            if (answerNumbers.indexOf(userNumber) == userNumbers.indexOf(userNumber)) {
+                strike++;
+            }
+        }
+    }
+
+    private void ballCount(String userNumbers, String answerNumbers) {
+        for (int i = 0; i < 3; i++) {
+            if (answerNumbers.contains(Character.toString(userNumbers.charAt(i)))) {
+                ball++;
+            }
+        }
+    }
+
+    public void resetCount() {
+        ball = 0;
+        strike = 0;
+    }
+
+
 }
--- src/main/java/domain/Refree.java
+++ src/main/java/domain/Refree.java
@@ -1,4 +1,20 @@
 package domain;
 
 public class Refree {
+    private final BaseballGame baseballGame;
+
+    public Refree() {
+        baseballGame = new BaseballGame();
+    }
+
+    public int[] getNumberCompare(String userNumbers, String answerNumbers) {
+        return baseballGame.getNumberCompareResult(userNumbers, answerNumbers);
+    }
+
+    public boolean isGameSet() {
+        if (baseballGame.getStrike() == 3) {
+            return true;
+        }
+        return false;
+    }
 }
